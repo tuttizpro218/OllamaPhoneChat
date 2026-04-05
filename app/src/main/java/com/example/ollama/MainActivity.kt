7b3a6f4558483831532e39bae04856b51298999a
tuttifrutti.top <dj_team10@hotmail.com>
2026-04-05 02:19:07 +0200

    Refactor WebView settings and add WebChromeClient
    Refactored WebView settings for better organization and added WebChromeClient.

--- app/src/main/java/com/example/ollama/MainActivity.kt
+++ app/src/main/java/com/example/ollama/MainActivity.kt
@@ -4,17 +4,32 @@ import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.webkit.WebChromeClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WebView.setWebContentsDebuggingEnabled(true)
+
         val webView = WebView(this)
         setContentView(webView)
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
-        webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+
+        with(webView.settings) {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            allowFileAccess = true
+            allowContentAccess = true
+            allowFileAccessFromFileURLs = true
+            allowUniversalAccessFromFileURLs = true
+            cacheMode = WebSettings.LOAD_NO_CACHE
+            databaseEnabled = true
+        }
+
         webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
         webView.loadUrl("file:///android_asset/index.html")
     }
 }
